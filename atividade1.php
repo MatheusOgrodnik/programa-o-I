33e3ef7f173e0760abca98d2fa9a3ab05719c074
MatheusOgrodnik <48133534+MatheusOgrodnik@users.noreply.github.com>
2019-03-01 14:49:55 -0300

    Add files via upload

---
+++ atividade1.php
@@ -0,0 +1,11 @@
+<?php
+    print "Digite a metragem (em metros)";
+    $metragem = (float) fgets(STDIN);
+
+
+    $milimetros = $metragem * 1000;
+    
+
+
+    print"Seu valor em milímetros é : $milimetros";
+
